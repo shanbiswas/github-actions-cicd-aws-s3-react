5298a919d6cc4afade1f64b8531e668c040f0ead
Santanu Biswas <shanpro.2015@gmail.com>
2025-02-04 22:59:47 +0530

    change text

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello React
-        </p>
+        <p>Hello React</p>
+        <p>Welcome to CICD</p>
         <a
           className="App-link"
           href="https://reactjs.org"
