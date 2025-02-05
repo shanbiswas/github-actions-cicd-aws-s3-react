b7d6d8a0422def8d43c88b1db09b4dd146a192d0
Santanu Biswas <shanpro.2015@gmail.com>
2025-02-05 12:51:11 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.REACT_APP_BASENAME || "/"}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
